db459975df7a8a10b5702b624b8ef5995d597a80
matheusnascimentoti99 <matheusnascimentoti99@gmail.com>
2022-09-17 21:01:26 -0300

    :bug: Remove type of return

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -6,20 +6,20 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
-    public function currentDate(Request $request): string
+    public function currentDate(Request $request)
     {
         return response()->json([
             "data" => now()
         ]);
     }
 
-    public function submit(Request $request): string
+    public function submit(Request $request)
     {
         $request->validate([
             'description' => 'required | string | max:255'
         ]);
         return response()->json([
             'data' => $request->description
-        ]);  
+        ]);
     }
 }
